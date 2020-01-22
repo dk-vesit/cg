dc1baa85d2d302dbc6a010555074557c236c4616
DK <darshankhot@programmer.net>
2020-01-22 18:35:24 +0530

    ignore output files

--- p1/dda.cpp
+++ p1/dda.cpp
@@ -6,7 +6,7 @@ main() {
 
 	int gd = DETECT, gm;
 	int x1 = 100, y1 = 100, x2 = 200, y2 = 200;
-	initgraph(&gd, &gm, NULL);
+	initgraph(&gd, &gm, NULL /* "C:\\TC\\BGI" */);
 
 	// Calculate dx, dy
 	int dx = x2 - x1;
