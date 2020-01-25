2ad456e6fdff8afe2ed6bd0382f73171ca879ec6
DK <darshankhot@programmer.net>
2020-01-25 12:47:37 +0530

    not created by DK

--- p1/bresenham.cpp
+++ p1/bresenham.cpp
@@ -1,12 +1,33 @@
 #include<graphics.h>
-main() {
-  int gd=DETECT, gm;
-  int x1 = 100, y1 = 100, x2 = 200, y2 = 200;
-  initgraph(&gd, &gm, NULL /*"C:\\TC\\BGI"*/);
-
-  
-
-  getch();
-  closegraph();
-  return 0;
+void drawline(int x0, int y0, int x1, int y1)
+{
+    int dx, dy, p, x, y;
+    dx=x1-x0;
+    dy=y1-y0;
+    x=x0;
+    y=y0;
+    p=2*dy-dx;
+    while(x<x1)
+    {
+        if(p>=0)
+        {
+            putpixel(x,y,15);
+            y=y+1;
+            p=p+2*dy-2*dx;
+        }
+        else
+        {
+            putpixel(x,y,15);
+            p=p+2*dy;}
+            x=x+1;
+        }
+}
+int main()
+{
+    int gdriver=DETECT, gmode, /*error,*/ x0=100, y0=200, x1=400, y1=400;
+    initgraph(&gdriver, &gmode, NULL /*"C:\\TC\\BGI"*/);
+    drawline(x0, y0, x1, y1);
+    getch();
+    closegraph();
+    return 0;
 }
